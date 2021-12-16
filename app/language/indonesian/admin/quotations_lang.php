dcf4a6c44d68b231901bce5df74bcbdfea483b70
tempebener <bayoewa@gmail.com>
2021-12-16 09:01:16 +0700

    First Up

---
+++ app/language/indonesian/admin/quotations_lang.php
@@ -0,0 +1,41 @@
+<?php
+
+defined('BASEPATH') or exit('No direct script access allowed');
+
+/*
+ * Language: Indonesia
+ * Module: Pesanans
+ *
+ * Last edited:
+ * 8th November 2018
+ *
+ * Package:
+ * Stock Manage Advance v3.0
+ *
+ * Translated by:
+ * Bram Andrian (barulaku) bram.andrian@gmail.com
+ *
+ * You can translate this file to your language.
+ * For instruction on new language setup, please visit the documentations.
+ * You also can share your language files by emailing to saleem@wmeat.store
+ * Thank you
+ */
+
+$lang['add_quote']          = 'Tambah Pesanan';
+$lang['edit_quote']         = 'Ubah Pesanan';
+$lang['delete_quote']       = 'Hapus Pesanan';
+$lang['delete_quotes']      = 'Hapus Pesanan - Pesanan';
+$lang['quote_added']        = 'Pesanan sukses ditambahkan';
+$lang['quote_updated']      = 'Pesanan sukses diupdate';
+$lang['quote_deleted']      = 'Pesanan sukses dihapus';
+$lang['quotes_deleted']     = 'Pesanans sukses dihapus';
+$lang['quote_details']      = 'Detail Pesanan';
+$lang['email_quote']        = 'Email Pesanan';
+$lang['view_quote_details'] = 'Lihat Detail Pesanan';
+$lang['quote_no']           = 'No Pesanan';
+$lang['send_email']         = 'Kirim Email';
+$lang['quote_items']        = 'Pesanan Barang';
+$lang['no_quote_selected']  = 'Tidak ada Pesanan yang dipilih. Silakan memilih setidaknya satu Pesanan.';
+$lang['create_sale']        = 'Membuat Penjualan';
+$lang['create_purchase']    = 'Membuat Pembelian';
+$lang['create_invoice']     = 'Membuat Faktur';
